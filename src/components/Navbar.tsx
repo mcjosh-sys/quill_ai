39016a7cb0fd8fca67ebc50d34d1475f778b080b
Olushola Joshua <olusholajoshua7@gmail.com>
2024-01-15 15:06:34 -0600

    initial commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link"
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import { buttonVariants } from "./ui/button"
-import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server"
+import { LoginLink, RegisterLink, getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { ArrowRight } from "lucide-react"
+import UserAccountNav from '@/components/UserAccountNav'
 
-const Navbar = () => {
+const Navbar = async () => {
+
+    const user = await getKindeServerSession().getUser()
+
+
     return (
         <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
             <MaxWidthWrapper>
@@ -15,7 +20,7 @@ const Navbar = () => {
                     {/* Todo: add mobile navbar */}
 
                     <div className="hidden items-center space-center space-x-4 sm:flex">
-                        <>
+                        {!user ? <>
                             <Link href='/pricing' className={buttonVariants({
                                 variant: 'ghost',
                                 size: 'sm'
@@ -29,7 +34,18 @@ const Navbar = () => {
                             <RegisterLink className={buttonVariants({
                                 size: 'sm'
                             })}>Get Started <ArrowRight className="ml-1.5 h-5 w-5" /></RegisterLink>
-                        </>
+                        </> : <>
+                            <Link href='/dashboard' className={buttonVariants({
+                                variant: 'ghost',
+                                size: 'sm'
+                            })}>Dashboard</Link>
+                            <UserAccountNav name={
+                                !user.given_name || !user.family_name ? "Your Account" : `${user.given_name} ${user.family_name}`
+                            }
+                                email={user.email ?? ''}
+                                imageUrl={user.picture ?? ''}
+                            />
+                        </>}
                     </div>
                 </div>
             </MaxWidthWrapper>
